63bde1d4a65f4a4708d5648896fd51a53eee99bc
Abdullah Habes <abdulla096@hotmail.com>
2021-11-04 00:29:18 -0400

    add movements

--- input.js
+++ input.js
@@ -2,13 +2,30 @@ const setupInput = function(connection) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
-  stdin.resume();
   stdin.on('data', (data) => {
     if (data === '\u0003') {
       process.exit();
     }
-    connection.write(data);
+    let command = "";
+    switch (data) {
+    case 'w':
+      command = "Move: up";
+      break;
+    case 'a':
+      command = "Move: left";
+      break;
+    case 's':
+      command = "Move: down";
+      break;
+    case 'd':
+      command = "Move: right";
+      break;
+    }
+    connection.write(command);
+    console.log(data);
   });
+  stdin.resume();
+
   return stdin;
 };
 

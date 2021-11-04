4c2badbbc3594fa34e829314e55e9c78b11ee8e9
Abdullah Habes <abdulla096@hotmail.com>
2021-11-03 23:56:08 -0400

    add autopilot mode

--- client.js
+++ client.js
@@ -1,14 +1,8 @@
 const net = require('net');
 
-const connect = (host, port, name) => {
-
+const connect = (host, port) => {
   const connection = net.createConnection({host: host, port: port});
-
   connection.setEncoding('utf-8');
-  connection.on('connect', () => {
-    console.log('Successfully connected to game server');
-    connection.write('Name: ' + name);
-  });
   return connection;
 };
 
--- play.js
+++ play.js
@@ -1,6 +1,26 @@
 const {connect} = require('./client');
 
-const connection = connect('165.227.47.243', 50541, 'AHS');
+const autoPilot = () => {
+  connection.on('connect', () => {
+    let delay = 0;
+    console.log('Moving the snake');
+    const moves = ["Move: up", "Move: right", "Move: right", "Move: right", "Move: down", "Move: left"];
+    moves.forEach(move => {
+      setTimeout(() => {
+        connection.write(move);
+        console.log(move);
+      }, delay += 1000);
+    });
+  });
+};
+const connection = connect('165.227.47.243', 50541);
+
+connection.on('connect', () => {
+  console.log('Successfully connected to game server');
+  connection.write('Name: AHS');
+});
+
+autoPilot();
 
 connection.on('data', data => {
   console.log('Message from server: ', data);
